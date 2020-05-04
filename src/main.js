4a2a0aea079a526ef2cb953fc91921b9a5d8e689
cw chen <sky85245630@gmail.com>
2020-05-05 01:25:22 +0800

    router.beforeEach requiresAuth and redirect login

--- src/main.js
+++ src/main.js
@@ -11,3 +11,27 @@ new Vue({
   router,
   render: h => h(App)
 }).$mount('#app')
+
+router.beforeEach((to, from, next) => {
+  console.log('to',to,'from',from,'next',next);
+  
+  if(to.meta.requiresAuth){
+    const api = `https://vue-course-api.herokuapp.com/api/user/check`; // 'http://localhost:3000/api/casper/products';
+      axios.post(api).then((response) => {
+        console.log(response.data);
+        if(response.data.success){
+          next();
+        }else {
+          next({
+            path:'/login'
+            
+          })
+        }
+      });
+  }else{
+    // alert('no need Auth')
+    next();
+  }
+  
+  next();
+})
