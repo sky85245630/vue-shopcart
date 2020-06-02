92877a62e54ee2b5dbad8749f502a0cc0bfaabcc
cw chen <sky85245630@gmail.com>
2020-06-02 23:53:11 +0800

    delProduct

--- src/views/products.vue
+++ src/views/products.vue
@@ -31,13 +31,36 @@
             <span v-else>未啟用</span>
           </td>
           <td>
-              <button class="btn btn-outline-primary btn-sm" @click="delete_item(item)">刪除</button>
+              <button class="btn btn-outline-primary btn-sm" @click="openDelProductModal(item)">刪除</button>
             <button class="btn btn-outline-primary btn-sm" @click="openModal(false,item)">編輯</button>
           </td>
         </tr>
       </tbody>
     </table>
     <!-- Modal -->
+    <div class="modal fade" id="delProductModal" tabindex="-1" role="dialog"
+      aria-labelledby="exampleModalLabel" aria-hidden="true">
+      <div class="modal-dialog" role="document">
+        <div class="modal-content border-0">
+          <div class="modal-header bg-danger text-white">
+            <h5 class="modal-title" id="exampleModalLabel">
+              <span>刪除產品</span>
+            </h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body">
+            是否刪除 <strong class="text-danger">{{ tempProduct.title }}</strong> 商品(刪除後將無法恢復)。
+          </div>
+          <div class="modal-footer">
+            <button type="button" class="btn btn-outline-secondary" data-dismiss="modal">取消</button>
+            <button type="button" class="btn btn-danger"
+              @click="delProduct">確認刪除</button>
+          </div>
+        </div>
+      </div>
+    </div>
     <div
       class="modal fade"
       id="productModal"
@@ -220,25 +243,21 @@ export default {
         }
       });
     },
-    delete_item(item){
-        const vm = this;
-
-        let api = `https://vue-course-api.herokuapp.com/api/louie/admin/product/${vm.tempProduct.id}`;
-        console.log(api)
-        this.tempProduct = Object.assign({},item)
-        this.$http.delete(api).then(response => {
-            console.log(response.data);
-            if(response.data.success){
-                vm.getProducts();
-                console.log('刪除成功')
-            }else{
-                vm.getProducts();
-                console.log('刪除失敗')
-            }
-        });
-
-
-    }
+    openDelProductModal(item) {
+      const vm = this;
+      $('#delProductModal').modal('show');
+      vm.tempProduct = Object.assign({}, item);
+    },
+    delProduct() {
+      const vm = this;
+      const url = `https://vue-course-api.herokuapp.com/api/louie/admin/product/${vm.tempProduct.id}`;
+      this.$http.delete(url).then((response) => {
+        console.log(response, vm.tempProduct);
+        $('#delProductModal').modal('hide');
+        vm.isLoading = false;
+        this.getProducts();
+      });
+    },
   },
   created() {
     this.getProducts();
